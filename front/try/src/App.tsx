4edaa2142769c3fb79eb1a193e6b254b597e2273
Nutzer <raulcattelan11@gmail.com>
2021-05-12 23:00:28 +0200

    first

---
+++ front/try/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {GlobalStyles} from './styles/GlobalStyles';
+import Page from './components/Page/index';
+
+function App() {
+  return (
+    <div className="App">
+<h1>rocket</h1>
+<GlobalStyles />
+
+<Page />
+    </div>
+  );
+}
+
+export default App;
---
+++ front/try/src/components/Page/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+//import {Container} from './styles';
+
+
+const Page: React.FC = ()=>{
+
+    return (
+        <div>
+<h1>try</h1>            
+        </div>
+    );
+}
+
+export default Page;
